e452625acb53a0e828dfcbe5c3382300c0cf8bef
luissfernann <luisfernandomagnum@gmail.com>
2025-10-19 12:55:55 -0300

    Criação da classe posição. Além disso: criação de vez dos atributos da classe personagem, do construtor, e dos métodos get e set para os seus atributos privados, e dependentes de outras classes ( casa e posiçao ).

---
+++ src/main/java/domain/Personagem.java
@@ -0,0 +1,41 @@
+package domain;
+
+public class Personagem {
+
+    private String nome;
+    private Casa casa;
+
+    private Posicao posicao;
+    private int vidaAtual;
+
+    public Personagem(String nome, Casa casa, Posicao posicao) {
+        this.nome = nome;
+        this.casa = casa;
+        this.posicao = posicao;
+        // a posição é passada, apesar de que ela poderá ser inicialmente definida pelo tabuleiro
+        this.vidaAtual = casa.getVidaMaxima();
+    }
+
+    public String getNomePersonagem() {
+        return nome;
+    }
+
+    public Casa getCasaPersonagem() {
+        return casa;
+    }
+
+    public Posicao getPosicaoPersonagem() {
+        return posicao;
+    }
+
+    public int getVidaAtual() {
+        return vidaAtual;
+    }
+
+    public Posicao setPosicaoPersonagem(Posicao posicao) {
+        this.posicao = posicao;
+        return posicao;
+    }
+
+
+}
