b1270c9482468198906cef084d6d939c1916098f
luissfernann <luisfernandomagnum@gmail.com>
2025-10-19 12:22:29 -0300

    atualizacao geral

--- src/main/java/domain/Casa.java
+++ src/main/java/domain/Casa.java
@@ -2,22 +2,6 @@ package domain;
 
 public class Casa {
 
-    /*
-
-    Podemos criar uma classe Casa, herdando variáveis fixas da casa, e subclasses, que seriam
-    Extents, ou melhor, heranças, da classe Casa, onde cada subclasse seria uma casa. Aí nesse caso
-    teríamos, por exemplo, a CasaStark extends Casa, CasaLannister extends Casa...
-
-    Nesse caso, teríamos algo para cada personagem herdar fixamente.
-
-    */
-
-    // atributos fixos seguindo a tabela 1 do documento.
-
-    // DEFINIÇÃO FIXA ( constantes ) DE VALORES : CADA CASA
-
-    // 1)
-
     private static final Casa STARK;
     private static final Casa LANNISTER;
     private static final Casa TARGARYEN;
@@ -29,20 +13,15 @@ public class Casa {
     }
 
 
-    public String nome;
-    public int vidaMaxima;
-    public int ataqueBase;
-    public int defesaBase;
-    public int alcanceMaximo;
+    private String nome;
+    private int vidaMaxima;
+    private int ataqueBase;
+    private int defesaBase;
+    private int alcanceMaximo;
 
-    // ainda não entendi para que serve a variável abaixo
-    public double modificadorOfensivo;
-    public double modificadorDefensivo;
-
-        // como cada personagem pertence a uma casa, e cada casa
-            // tem suas variáveis fixas, seguimos o construtor
-
-    // 2 )
+    // ainda não entendi para que serve a variável abaixo: vamos estudar melhor seu uso
+    private double modificadorOfensivo;
+    private double modificadorDefensivo;
 
     public Casa (String nome, int vidaMaxima, int ataqueBase, int defesaBase, int alcanceMaximo, double modificadorOfensivo, double modificadorDefensivo) {
         this.nome = nome;
@@ -54,8 +33,6 @@ public class Casa {
         this.modificadorDefensivo = modificadorDefensivo;
 
     }
-
-    // retornando os atributos que os personagens devem ter acesso
 
     public String getNome() {
         return nome;
