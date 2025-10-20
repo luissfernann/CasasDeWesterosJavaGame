03464c6652937db98944834f190efb3e44e7ffc6
Gabriel Ernesto Leal de Bittencourt <gabobittencourt@gmail.com>
2025-10-19 22:53:06 -0300

    Dando Forma ao Tabuleiro (ainda falta adicionar coisas na classe)

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,40 @@
 package app;
 
+import domain.Personagem;
+
 public class Main {
 
     static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+}
+
+public class Tabuleiro{
+
+    private Personagem[][] grade;
+    private final  int TAMANHO = 10;
+
+
+public Tabuleiro(){
+
+this.grade = new Personagem[TAMANHO][TAMANHO];
+
+
+}
+public void posicicaoPInicial(Personagem[] time1, Personagem[] time2){
+
+}
+
+
+//Falta fazer uma booleana para mover os personagens
+
+    public void ImprimeTabuleiro(){
+
+
+
+    }
+
+
 
 }
