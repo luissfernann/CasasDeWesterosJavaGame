15df61aa311a6e1afb7ac8f14d24e8047deef16d
archanjo14 <josearchanjo15@gmail.com>
2025-10-22 17:24:56 -0300

    comeco da escrita da posição pelo archanjo
    Fix random number generation and import statement.

--- src/main/java/domain/Posicao.java
+++ src/main/java/domain/Posicao.java
@@ -1,19 +1,27 @@
 package domain;
-
+import java.until.Random;
+    
 public class Posicao {
+ private int linha;
+ private int coluna;
 
-
-
+  
+public Posicao(int linha,int  coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+}
+    public Posicao(int minLinha, int maxLinha, int tamanhoTabuleiro) {
+        Random random = new random();
+        this.linha = random nextInt(maxLinha - minLinha + 1) + minLinha;
+        this.coluna = random.nextInt(tamanhoTabuleiro);
+    }
     public int getLinha() {
         return linha;
     }
-
     public int getColuna() {
         return coluna;
-
     }
-
-
+    
     public void setPosicao(int novalinha, int novaColuna) {
 
         this.linha = novalinha;
